ec29a99334c9e9aefb9c0b601592b59b7418c84a
DiegoRojo99 <Diego99rojo@gmail.com>
2020-11-18 12:17:40 +0100

    cambios

--- top100/urls.py
+++ top100/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
    path('artista', views.artista, name='artista'), #todos los artistas
    path('album', views.album, name='album'), #todos los albums
    path('estilo', views.estilo, name='estilo'), #todos los estilos
-   path('cancion/<int:cancion_id>/cancionPost', views.detailCancion, name='detailCancion'), #detalles de canciones
-   path('artista/<int:artista_id>/detailArtistas', views.detailArtista, name='detailArtistaS'), #detalles de artistas
-   path('album/<int:album_id>/detailAlbum', views.detailAlbum, name='detailAlbum'), #detalles de albumes
-   path('estilo/<int:estilo_id>/detailEstilo', views.detailEstilo, name='detailEstilo') #detalles de estilos
+   path('cancion/<int:cancion_id>', views.detailCancion, name='detailCancion'), #detalles de canciones
+   path('artista/<int:artista_id>', views.detailArtista, name='detailArtistaS'), #detalles de artistas
+   path('album/<int:album_id>', views.detailAlbum, name='detailAlbum'), #detalles de albumes
+   path('estilo/<int:estilo_id>', views.detailEstilo, name='detailEstilo') #detalles de estilos
    #path('acerca', views.acerca, name= 'acerca'),
    #path('contacto', views.contacto, name = 'contacto')
     
