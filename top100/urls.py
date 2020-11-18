bc657898322912ee332252c8c085a951bd24b862
mikelgomez <mikelgomez@opendeusto.es>
2020-11-18 22:19:03 +0100

    modificada la estructura de la plantilla. Implementacion de herencia

--- top100/urls.py
+++ top100/urls.py
@@ -3,14 +3,20 @@ from . import views
 
 urlpatterns = [
    path('', views.index, name='index'),
-   path('cancion', views.index, name='cancion'), #todas las canciones
-   path('artista', views.artista, name='artista'), #todos los artistas
-   path('album', views.album, name='album'), #todos los albums
-   path('estilo', views.estilo, name='estilo'), #todos los estilos
-   path('cancion/<int:cancion_id>', views.detailCancion, name='detailCancion'), #detalles de canciones
-   path('artista/<int:artista_id>', views.detailArtista, name='detailArtistaS'), #detalles de artistas
-   path('album/<int:album_id>', views.detailAlbum, name='detailAlbum'), #detalles de albumes
-   path('estilo/<int:estilo_id>', views.detailEstilo, name='detailEstilo') #detalles de estilos
+   path('featured/', views.featured, name='featured'),
+   path('join/', views.join, name='join'),
+   path('portfolio/', views.port, name='port'),
+   path('eventos/', views.eventos, name='eventos'),
+   path('equipo/', views.equipo, name='equipo'),
+   path('contacto/', views.contacto, name='contacto'),
+   #path('cancion', views.index, name='cancion'), #todas las canciones
+   #path('artista', views.artista, name='artista'), #todos los artistas
+   #path('album', views.album, name='album'), #todos los albums
+   #path('estilo', views.estilo, name='estilo'), #todos los estilos
+   #path('cancion/<int:cancion_id>', views.detailCancion, name='detailCancion'), #detalles de canciones
+   #path('artista/<int:artista_id>', views.detailArtista, name='detailArtistaS'), #detalles de artistas
+   #path('album/<int:album_id>', views.detailAlbum, name='detailAlbum'), #detalles de albumes
+   #path('estilo/<int:estilo_id>', views.detailEstilo, name='detailEstilo'), #detalles de estilos
    #path('acerca', views.acerca, name= 'acerca'),
    #path('contacto', views.contacto, name = 'contacto')
     
--- top100/views.py
+++ top100/views.py
@@ -4,45 +4,71 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, get_list_or_404
 from .models import Artista, Album, Estilo, Cancion
 
-def index(request):#canciones
-	return render(request, 'index.html')
+
+
+def index(request):
+	return render(request, 'principal.html')
+
+def featured(request):
+	return render(request, 'featured.html')
+
+def join(request):
+	return render(request, 'join.html')
+
+def port(request):
+	return render(request, 'portfolio.html')
+
+def eventos(request):
+	return render(request, 'eventos.html')
+
+def equipo(request):
+	return render(request, 'equipo.html')
+
+def contacto(request):
+	return render(request, 'contacto.html')
+
+
+
+
+#def index(request):#canciones
+#	return render(request, 'index.html')
 
 #def index(request):#canciones
 #	canciones = get_list_or_404(Cancion.objects.order_by('id'))
 #	context = {'lista_canciones': canciones }
 #	return render(request, 'index.html', context)
 
-def detailCancion(request, post_id):#Para ir desde canciones a la cancion especifica
-    cancion = get_object_or_404(Cancion, pk=cancion_id)
-    context = {'cancion': cancion}
-    return render(request, 'detailCancion.html', context) #detail
+#def detailCancion(request, post_id):#Para ir desde canciones a la cancion especifica
+#    cancion = get_object_or_404(Cancion, pk=cancion_id)
+#    context = {'cancion': cancion}
+#    return render(request, 'detailCancion.html', context) #detail
 
-def artista(request):#artistas
-	artistas = get_list_or_404(Artista.objects.order_by('id'))
-	context = {'lista_artistas': artistas }
-	return render(request, 'artista.html', context)
+#def artista(request):#artistas
+#	artistas = get_list_or_404(Artista.objects.order_by('id'))
+#	context = {'lista_artistas': artistas }
+#	return render(request, 'artista.html', context)
 
-def detailArtista(request, artista_id):#Para ir desde los artistas al artista
-    artista = get_object_or_404(Artista, pk=artista_id)
-    context = {'artista': artista}
-    return render(request, 'detailArtista.html', context)
+#def detailArtista(request, artista_id):#Para ir desde los artistas al artista
+#    artista = get_object_or_404(Artista, pk=artista_id)
+#    context = {'artista': artista}
+#    return render(request, 'detailArtista.html', context)
 
-def album(request):#albums
-	albums = get_list_or_404(Album.objects.order_by('id'))
-	context = {'lista_albums': albums }
-	return render(request, 'album.html', context)
+#def album(request):#albums
+#	albums = get_list_or_404(Album.objects.order_by('id'))
+#	context = {'lista_albums': albums }
+#	return render(request, 'album.html', context)
 
-def detailAlbum(request, album_id):#Para ir desde los albums al album
-    album = get_object_or_404(Album, pk=album_id)
-    context = {'album': album}
-    return render(request, 'detailAlbum.html', context)
+#def detailAlbum(request, album_id):#Para ir desde los albums al album
+#    album = get_object_or_404(Album, pk=album_id)
+#    context = {'album': album}
+#    return render(request, 'detailAlbum.html', context)
 
-def estilo(request):#estilos
-	estilos = get_list_or_404(Estilo.objects.order_by('id'))
-	context = {'lista_estilos': estilos }
-	return render(request, 'estilo.html', context)
+#def estilo(request):#estilos
+#	estilos = get_list_or_404(Estilo.objects.order_by('id'))
+#	context = {'lista_estilos': estilos }
+#	return render(request, 'estilo.html', context)
 
-def detailEstilo(request, estilo_id):#Para ir desde los estilos al estilo
-    estilo = get_object_or_404(Estilo, pk=estilo_id)
-    context = {'estilo': estilo}
-    return render(request, 'detailEstilo.html', context)
+#def detailEstilo(request, estilo_id):#Para ir desde los estilos al estilo
+#    estilo = get_object_or_404(Estilo, pk=estilo_id)
+#    context = {'estilo': estilo}
+#    return render(request, 'detailEstilo.html', context)
