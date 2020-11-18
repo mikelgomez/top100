e678030bbeb414e30c3ab045866d9ec225c16fd8
DiegoRojo99 <Diego99rojo@gmail.com>
2020-11-18 09:34:01 +0100

    Modelos en admin

--- top100/admin.py
+++ top100/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cancion, Album, Estilo, Artista
 
 # Register your models here.
+admin.site.register(Cancion)
+admin.site.register(Album)
+admin.site.register(Estilo)
+admin.site.register(Artista)
