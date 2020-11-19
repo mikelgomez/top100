8faa2fadb7c8d7d6553e3b010af864769dbd0afe
DiegoRojo99 <Diego99rojo@gmail.com>
2020-11-19 23:19:05 +0100

    Añadidos estilos

--- top100/urls.py
+++ top100/urls.py
@@ -7,16 +7,16 @@ urlpatterns = [
    path('albumes/<int:album_id>', views.detailAlbum, name='detailAlbum'), #detalles de albumes
    path('artistas/', views.artistas, name='artistas'),
    path('artistas/<int:artista_id>', views.detailArtista, name='detailArtista'), #detalles de artistas
-   path('portfolio/', views.port, name='port'),
+   path('estilos/', views.estilos, name='estilos'),
+   path('estilos/<int:estilo_id>', views.detailEstilo, name='detailEstilo'), #detalles de estilos
    path('eventos/', views.eventos, name='eventos'),
    path('equipo/', views.equipo, name='equipo'),
    path('contacto/', views.contacto, name='contacto'),
    #path('cancion', views.index, name='cancion'), #todas las canciones
    #path('artista', views.artista, name='artista'), #todos los artistas
    #path('album', views.album, name='album'), #todos los albums
-   #path('estilo', views.estilo, name='estilo'), #todos los estilos
    #path('cancion/<int:cancion_id>', views.detailCancion, name='detailCancion'), #detalles de canciones
-   #path('estilo/<int:estilo_id>', views.detailEstilo, name='detailEstilo'), #detalles de estilos
+  
    #path('acerca', views.acerca, name= 'acerca'),
    #path('contacto', views.contacto, name = 'contacto')
     
--- top100/views.py
+++ top100/views.py
@@ -30,8 +30,17 @@ def detailArtista(request, artista_id):#Para ir desde los artistas al artista
 	context = {'artista': artista, 'albumes': albumes}
 	return render(request, 'detailArtista.html', context)
 
-def port(request):
-	return render(request, 'portfolio.html')
+def estilos(request):
+	estilos = get_list_or_404(Estilo.objects.order_by('nombreEstilo'))
+	albumes = get_list_or_404(Album.objects.order_by('estiloAlbum'))
+	context = {'estilos': estilos, 'albumes': albumes}
+	return render(request, 'estilos.html', context)
+
+def detailEstilo(request, estilo_id):#Para ir desde los estilos al estilo
+	estilo = get_object_or_404(Estilo, pk=estilo_id)
+	albumes = get_list_or_404(Album.objects.order_by('nombreAlbum'), estiloAlbum=estilo)
+	context = {'estilo': estilo, 'albumes' : albumes}
+	return render(request, 'detailEstilo.html', context)
 
 def eventos(request):
 	return render(request, 'eventos.html')
@@ -57,33 +66,3 @@ def contacto(request):
 #    cancion = get_object_or_404(Cancion, pk=cancion_id)
 #    context = {'cancion': cancion}
 #    return render(request, 'detailCancion.html', context) #detail
-
-#def artista(request):#artistas
-#	artistas = get_list_or_404(Artista.objects.order_by('id'))
-#	context = {'lista_artistas': artistas }
-#	return render(request, 'artista.html', context)
-
-#def detailArtista(request, artista_id):#Para ir desde los artistas al artista
-#    artista = get_object_or_404(Artista, pk=artista_id)
-#    context = {'artista': artista}
-#    return render(request, 'detailArtista.html', context)
-
-#def album(request):#albums
-#	albums = get_list_or_404(Album.objects.order_by('id'))
-#	context = {'lista_albums': albums }
-#	return render(request, 'album.html', context)
-
-#def detailAlbum(request, album_id):#Para ir desde los albums al album
-#    album = get_object_or_404(Album, pk=album_id)
-#    context = {'album': album}
-#    return render(request, 'detailAlbum.html', context)
-
-#def estilo(request):#estilos
-#	estilos = get_list_or_404(Estilo.objects.order_by('id'))
-#	context = {'lista_estilos': estilos }
-#	return render(request, 'estilo.html', context)
-
-#def detailEstilo(request, estilo_id):#Para ir desde los estilos al estilo
-#    estilo = get_object_or_404(Estilo, pk=estilo_id)
-#    context = {'estilo': estilo}
-#    return render(request, 'detailEstilo.html', context)
